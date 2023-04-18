a44d96712379a3f2bb51f581258f31034f1ba318
Teppo Kurki <teppo.kurki@vaadin.com>
2023-04-18 12:02:09 +0300

    Add initial v8 demo app

---
+++ demo-vaadin-8/src/main/java/com/vaadin/appseckit/DemoUI.java
@@ -0,0 +1,31 @@
+package com.vaadin.appseckit;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("demouitheme")
+public class DemoUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        layout.addComponents(new Label("AppSec Kit Vaadin 8 Demo Application"));
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "DemoUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = DemoUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
