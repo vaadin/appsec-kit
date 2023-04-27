348c340caae4674fd9538cf87fa99de9e67f8869
tamasmak <maktamas@gmail.com>
2023-04-27 16:53:06 +0200

    Parse the SBOM resource with cyclonedx-core-java and list the components

---
+++ demo-vaadin-8/src/main/java/com/vaadin/appseckit/service/BOMService.java
@@ -0,0 +1,24 @@
+package com.vaadin.appseckit.service;
+
+import org.cyclonedx.model.Bom;
+
+public class BOMService {
+
+    private Bom bom;
+
+    private static final class InstanceHolder {
+        static final BOMService instance = new BOMService();
+    }
+
+    public static BOMService getInstance() {
+        return InstanceHolder.instance;
+    }
+
+    public void init(Bom bom) {
+        this.bom = bom;
+    }
+
+    public Bom getBom() {
+        return this.bom;
+    }
+}
