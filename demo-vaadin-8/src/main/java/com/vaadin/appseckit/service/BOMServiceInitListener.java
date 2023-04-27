e1454a64b95244ae804dd55177cb482ec9cdb796
tamasmak <maktamas@gmail.com>
2023-04-27 16:53:06 +0200

    Parse the SBOM resource with cyclonedx-core-java and list the components

---
+++ demo-vaadin-8/src/main/java/com/vaadin/appseckit/service/BOMServiceInitListener.java
@@ -0,0 +1,38 @@
+package com.vaadin.appseckit.service;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import org.cyclonedx.exception.ParseException;
+import org.cyclonedx.model.Bom;
+import org.cyclonedx.parsers.JsonParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.server.ServiceInitEvent;
+import com.vaadin.server.VaadinServiceInitListener;
+
+public class BOMServiceInitListener implements VaadinServiceInitListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BOMServiceInitListener.class);
+    private static final String BOM_PATH = "/resources/bom.json";
+
+    @Override
+    public void serviceInit(ServiceInitEvent event) {
+        URL resource = BOMServiceInitListener.class.getResource(BOM_PATH);
+        if (resource != null) {
+            JsonParser parser = new JsonParser();
+            try {
+                Bom bom = parser.parse(Paths.get(resource.toURI()).toFile());
+                BOMService.getInstance().init(bom);
+            } catch (URISyntaxException e) {
+                LOGGER.error("Syntax error in BOM resource path: " + BOM_PATH, e);
+            } catch (ParseException e) {
+                LOGGER.error("Can't parse the BOM resource. ", e);
+            }
+        } else {
+            LOGGER.error("Can't get BOM resource on path: " + BOM_PATH);
+        }
+    }
+}
