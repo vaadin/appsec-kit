bcdc56730d8cb17b070e49c32ba14515d2df34a4
tamasmak <maktamas@gmail.com>
2023-05-03 13:03:08 +0200

    Revert "Parse the SBOM resource with cyclonedx-core-java and list the components"
    This reverts commit e1454a64b95244ae804dd55177cb482ec9cdb796.

--- demo-vaadin-8/src/main/java/com/vaadin/appseckit/DemoUI.java
+++ demo-vaadin-8/src/main/java/com/vaadin/appseckit/DemoUI.java
@@ -2,15 +2,13 @@ package com.vaadin.appseckit;
 
 import javax.servlet.annotation.WebServlet;
 
-import org.cyclonedx.model.Component;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.appseckit.service.BOMService;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Grid;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -21,17 +19,7 @@ public class DemoUI extends UI {
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
         
-        layout.addComponent(new Label("AppSec Kit Vaadin 8 Demo Application"));
-
-        layout.addComponent(new Label("SBOM components:"));
-
-        Grid<Component> grid = new Grid<>();
-        grid.setItems(BOMService.getInstance().getBom().getComponents());
-        grid.addColumn(Component::getGroup).setCaption("Group");
-        grid.addColumn(Component::getName).setCaption("Name");
-        grid.addColumn(Component::getVersion).setCaption("Version");
-        grid.setSizeFull();
-        layout.addComponent(grid);
+        layout.addComponents(new Label("AppSec Kit Vaadin 8 Demo Application"));
         
         setContent(layout);
     }
