379e9da14ebf4a61c746332ba27ba08d6b2be986
Giovanni Lovato <giovanni@lova.to>
2023-06-27 11:16:27 +0200

    Add method to get new vulnerabilities from scan event

---
+++ appsec-kit-backend/src/test/java/com/vaadin/appsec/backend/AppSecScanEventTest.java
@@ -0,0 +1,65 @@
+/*-
+ * Copyright (C) 2023 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full license.
+ */
+package com.vaadin.appsec.backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.vaadin.appsec.backend.model.AppSecData;
+import com.vaadin.appsec.backend.model.AppSecData.Vulnerability;
+import com.vaadin.appsec.backend.model.dto.VulnerabilityDTO;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class AppSecScanEventTest {
+
+    private AppSecService service;
+
+    private AppSecData data;
+
+    private List<VulnerabilityDTO> vulnerabilities;
+
+    @Before
+    public void setup() {
+        service = Mockito.mock(AppSecService.class);
+        data = new AppSecData();
+        vulnerabilities = new ArrayList<>();
+        when(service.getData()).thenReturn(data);
+        when(service.getVulnerabilities()).thenReturn(vulnerabilities);
+    }
+
+    @Test
+    public void newVulnerabilties_noneExpected() {
+        vulnerabilities.add(new VulnerabilityDTO("foo"));
+        data.getVulnerabilities().put("foo", new Vulnerability());
+
+        AppSecScanEvent event = new AppSecScanEvent(service);
+        List<VulnerabilityDTO> newVulnerabilities = event
+                .getNewVulnerabilities();
+
+        assertEquals(0, newVulnerabilities.size());
+    }
+
+    @Test
+    public void newVulnerabilties_oneExpected() {
+        vulnerabilities.add(new VulnerabilityDTO("foo"));
+
+        AppSecScanEvent event = new AppSecScanEvent(service);
+        List<VulnerabilityDTO> newVulnerabilities = event
+                .getNewVulnerabilities();
+
+        assertEquals(1, newVulnerabilities.size());
+        assertEquals("foo", newVulnerabilities.get(0).getIdentifier());
+    }
+}
