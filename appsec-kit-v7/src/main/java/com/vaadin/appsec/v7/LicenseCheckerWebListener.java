00fabeaa8cd1025f54f7dc73f807dd6b428345eb
Giovanni Lovato <giovanni@lova.to>
2023-06-28 16:01:31 +0200

    Add license-check for Vaadin 7

---
+++ appsec-kit-v7/src/main/java/com/vaadin/appsec/v7/LicenseCheckerWebListener.java
@@ -0,0 +1,66 @@
+/*-
+ * Copyright (C) 2023 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
+package com.vaadin.appsec.v7;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import com.vaadin.pro.licensechecker.BuildType;
+import com.vaadin.pro.licensechecker.LicenseChecker;
+import com.vaadin.server.VaadinService;
+
+/**
+ * Session listener to verify the license.
+ */
+@WebListener
+public class LicenseCheckerWebListener implements HttpSessionListener {
+
+    static final String PROPERTIES_RESOURCE = "appsec-kit.properties";
+
+    static final String VERSION_PROPERTY = "appsec-kit.version";
+
+    static final String PRODUCT_NAME = "vaadin-appsec-kit";
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        VaadinService vaadinService = VaadinService.getCurrent();
+        if (vaadinService != null) {
+            checkLicense(vaadinService);
+        }
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        // NO-OP
+    }
+
+    private void checkLicense(VaadinService service) {
+        try {
+            final Properties properties = new Properties();
+            properties.load(LicenseCheckerWebListener.class.getClassLoader()
+                    .getResourceAsStream(PROPERTIES_RESOURCE));
+            final String version = properties.getProperty(VERSION_PROPERTY);
+
+            // Check the license at runtime if in development mode
+            if (!service.getDeploymentConfiguration().isProductionMode()) {
+                // Using a null BuildType to allow trial licensing builds
+                // The variable is defined to avoid method signature ambiguity
+                BuildType buildType = null;
+                LicenseChecker.checkLicense(PRODUCT_NAME, version, buildType);
+            }
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+}
