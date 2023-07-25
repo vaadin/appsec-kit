159c2d8b299efbdeb63ea1a47a699d7cc1b6fe21
tamasmak <maktamas@gmail.com>
2023-07-25 15:01:42 +0200

    Remove servlet configuration

--- appsec-kit-demo-v8-cdi/src/main/java/com/vaadin/appsec/demo/v8cdi/DemoUI.java
+++ appsec-kit-demo-v8-cdi/src/main/java/com/vaadin/appsec/demo/v8cdi/DemoUI.java
@@ -8,14 +8,10 @@
  */
 package com.vaadin.appsec.demo.v8cdi;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -33,10 +29,5 @@ public class DemoUI extends UI {
                 new Label("AppSec Kit Vaadin 8 CDI Demo Application"));
 
         setContent(layout);
-    }
-
-    @WebServlet(urlPatterns = "/*", name = "DemoUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = DemoUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
     }
 }
