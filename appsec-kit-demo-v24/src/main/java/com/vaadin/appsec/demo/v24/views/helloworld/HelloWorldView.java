f3066e52bae356953943a35b8a5a412c2da93bde
tamasmak <maktamas@gmail.com>
2023-08-17 03:36:42 +0200

    Add appsec-kit-v24 module and init appsec kit

--- appsec-kit-demo-v24/src/main/java/com/vaadin/appsec/demo/v24/views/helloworld/HelloWorldView.java
+++ appsec-kit-demo-v24/src/main/java/com/vaadin/appsec/demo/v24/views/helloworld/HelloWorldView.java
@@ -1,3 +1,11 @@
+/*-
+ * Copyright (C) 2023 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full license.
+ */
 package com.vaadin.appsec.demo.v24.views.helloworld;
 
 import com.vaadin.appsec.demo.v24.views.MainLayout;
