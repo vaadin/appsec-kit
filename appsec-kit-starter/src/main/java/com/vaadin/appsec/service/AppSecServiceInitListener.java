3cd06051410fd668f010005ed745c34c1d7e1458
tamasmak <maktamas@gmail.com>
2023-11-14 14:56:29 +0100

    Use one service init listener to initialize the service and the notifications

--- appsec-kit-starter/src/main/java/com/vaadin/appsec/service/AppSecServiceInitListener.java
+++ appsec-kit-starter/src/main/java/com/vaadin/appsec/service/AppSecServiceInitListener.java
@@ -8,39 +8,140 @@
  */
 package com.vaadin.appsec.service;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.vaadin.appsec.backend.AppSecScanEvent;
 import com.vaadin.appsec.backend.AppSecService;
+import com.vaadin.appsec.backend.Registration;
 import com.vaadin.appsec.views.AppSecView;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.AnchorTarget;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.RouteConfiguration;
 import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.SessionDestroyEvent;
+import com.vaadin.flow.server.UIInitEvent;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.server.VaadinSession;
 
 /**
  * A Vaadin service listener for registering the AppSec Kit route and
  * initializing AppSec Kit services. Will be invoked automatically by Vaadin.
  */
-public class AppSecServiceInitListener extends AbstractInitListener {
+public class AppSecServiceInitListener implements VaadinServiceInitListener {
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(AppSecServiceInitListener.class);
 
+    private final Map<UI, Registration> scanEventRegistrations = new ConcurrentHashMap<>();
+
     @Override
     public void serviceInit(ServiceInitEvent event) {
-        if (isDebugMode(event.getSource())) {
+        VaadinService service = event.getSource();
+        if (isDebugMode(service)) {
             AppSecService appSecService = AppSecService.getInstance();
+
             registerRoute(appSecService.getConfiguration().getAppSecRoute());
             appSecService.init();
+            service.addUIInitListener(this::subscribeUIToScanEvents);
+            service.addSessionDestroyListener(this::removeUIRegistrations);
             LOGGER.info("AppSec Kit initialized");
+
+            appSecService.scanForVulnerabilities()
+                    .thenRun(appSecService::scheduleAutomaticScan);
         } else {
             LOGGER.info("AppSec Kit not enabled in production mode. Run the "
                     + "application in debug mode to initialize AppSec Kit");
         }
     }
 
+    private boolean isDebugMode(VaadinService vaadinService) {
+        return !vaadinService.getDeploymentConfiguration().isProductionMode();
+    }
+
     private void registerRoute(String path) {
         RouteConfiguration configuration = RouteConfiguration
                 .forApplicationScope();
         configuration.setRoute(path, AppSecView.class);
+    }
+
+    private void removeUIRegistrations(SessionDestroyEvent event) {
+        VaadinSession session = event.getSession();
+        session.getUIs().forEach(ui -> {
+            Registration registration = scanEventRegistrations.get(ui);
+            if (registration != null) {
+                registration.remove();
+            }
+            scanEventRegistrations.remove(ui);
+            LOGGER.debug("Scan event listener removed");
+        });
+    }
+
+    private void subscribeUIToScanEvents(UIInitEvent event) {
+        scanEventRegistrations.computeIfAbsent(event.getUI(),
+                this::computeRegistration);
+        LOGGER.debug("Scan event listener added");
+    }
+
+    private Registration computeRegistration(UI ui) {
+        return AppSecService.getInstance().addScanEventListener(event -> {
+            if (!event.getNewVulnerabilities().isEmpty()) {
+                if (ui.getChildren().anyMatch(AppSecView.class::isInstance)) {
+                    return;
+                }
+                ui.access(() -> doNotifyUI(event));
+                LOGGER.debug("Notifying UI[" + ui.getUIId() + "]");
+            }
+        });
+    }
+
+    private void doNotifyUI(AppSecScanEvent event) {
+        Notification notification = new Notification();
+        notification.addThemeVariants(NotificationVariant.LUMO_CONTRAST);
+        notification.setPosition(Notification.Position.TOP_END);
+
+        int duration = (int) AppSecService.getInstance().getConfiguration()
+                .getAutoScanInterval().toMillis();
+        notification.setDuration(duration);
+
+        Div header = new Div(new Text("AppSec Kit"));
+        header.getStyle().set("font-weight", "600");
+
+        String infoStr = "%d vulnerabilities found.";
+        Text info = new Text(
+                String.format(infoStr, event.getNewVulnerabilities().size()));
+
+        Anchor open = new Anchor("/" + AppSecService.getInstance()
+                .getConfiguration().getAppSecRoute(), "Open AppSec Kit");
+        open.setTarget(AnchorTarget.BLANK);
+        Text text = new Text(" for details.");
+
+        Div content = new Div(header, new Div(info), new Div(open, text));
+
+        Button closeButton = new Button(new Icon(VaadinIcon.CLOSE_SMALL));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        closeButton.getElement().setAttribute("aria-label", "Close");
+        closeButton.addClickListener(buttonClickEvent -> notification.close());
+
+        HorizontalLayout layout = new HorizontalLayout(content, closeButton);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        notification.add(layout);
+        notification.open();
     }
 }
--- appsec-kit-starter/src/test/java/com/vaadin/appsec/service/AppSecServiceInitListenerTest.java
+++ appsec-kit-starter/src/test/java/com/vaadin/appsec/service/AppSecServiceInitListenerTest.java
@@ -8,25 +8,49 @@
  */
 package com.vaadin.appsec.service;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.vaadin.appsec.backend.AppSecConfiguration;
+import com.vaadin.appsec.backend.AppSecScanEvent;
+import com.vaadin.appsec.backend.AppSecScanEventListener;
 import com.vaadin.appsec.backend.AppSecService;
+import com.vaadin.appsec.backend.model.dto.Vulnerability;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.router.RouteConfiguration;
+import com.vaadin.flow.server.Command;
 import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.SessionDestroyEvent;
+import com.vaadin.flow.server.SessionDestroyListener;
+import com.vaadin.flow.server.UIInitEvent;
+import com.vaadin.flow.server.UIInitListener;
 import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinSession;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class AppSecServiceInitListenerTest {
 
     private AppSecServiceInitListener listener;
@@ -35,6 +59,16 @@ public class AppSecServiceInitListenerTest {
     private MockedStatic<AppSecService> appSecService;
     private AppSecService appSecServiceInstance;
     private MockedStatic<RouteConfiguration> routeConfiguration;
+    private MockedConstruction<Notification> notification;
+
+    @Captor
+    private ArgumentCaptor<UIInitListener> uiInitListenerCaptor;
+    @Captor
+    private ArgumentCaptor<SessionDestroyListener> sessionDestroyListenerCaptor;
+    @Captor
+    private ArgumentCaptor<AppSecScanEventListener> appSecScanEventListenerCaptor;
+    @Captor
+    private ArgumentCaptor<Command> commandCaptor;
 
     @BeforeEach
     public void setup() {
@@ -45,11 +79,7 @@ public class AppSecServiceInitListenerTest {
         when(vaadinService.getDeploymentConfiguration())
                 .thenReturn(deploymentConfiguration);
 
-        var appSecConfiguration = mock(AppSecConfiguration.class);
         appSecServiceInstance = mock(AppSecService.class);
-        when(appSecServiceInstance.getConfiguration())
-                .thenReturn(appSecConfiguration);
-
         appSecService = mockStatic(AppSecService.class);
         appSecService.when(AppSecService::getInstance)
                 .thenReturn(appSecServiceInstance);
@@ -58,21 +88,81 @@ public class AppSecServiceInitListenerTest {
         routeConfiguration = mockStatic(RouteConfiguration.class);
         routeConfiguration.when(RouteConfiguration::forApplicationScope)
                 .thenReturn(routeConfigurationInstance);
+
+        notification = mockConstruction(Notification.class);
     }
 
     @AfterEach
     public void cleanup() {
         appSecService.close();
         routeConfiguration.close();
+        notification.close();
     }
 
     @Test
     public void debugMode_appSecServiceInitialized() {
+        var ui = mock(UI.class);
+        UI.setCurrent(ui);
+        var vaadinSession = mock(VaadinSession.class);
+        when(vaadinSession.getUIs()).thenReturn(Collections.singletonList(ui));
+        var vulnerabilities = Arrays.asList(new Vulnerability("CVE-001"),
+                new Vulnerability("CVE-002"));
+        var appSecScanEvent = mock(AppSecScanEvent.class);
+        when(appSecScanEvent.getNewVulnerabilities())
+                .thenReturn(vulnerabilities);
         when(deploymentConfiguration.isProductionMode()).thenReturn(false);
+        var registration = mock(com.vaadin.flow.shared.Registration.class);
+        when(vaadinService.addUIInitListener(any())).thenReturn(registration);
+        when(vaadinService.addSessionDestroyListener(any()))
+                .thenReturn(registration);
+        var scanEventRegistration = mock(
+                com.vaadin.appsec.backend.Registration.class);
+        when(appSecServiceInstance.addScanEventListener(any()))
+                .thenReturn(scanEventRegistration);
+        var mockVoid = mock(Void.class);
+        when(appSecServiceInstance.scanForVulnerabilities())
+                .thenReturn((CompletableFuture.completedFuture(mockVoid)));
+        when(appSecServiceInstance.getConfiguration())
+                .thenReturn(new AppSecConfiguration());
 
         listener.serviceInit(new ServiceInitEvent(vaadinService));
 
+        // verifies if AppSec service is initialized
         verify(appSecServiceInstance, times(1)).init();
+
+        // verifies that a UI init listener is added
+        verify(vaadinService).addUIInitListener(uiInitListenerCaptor.capture());
+        var uiInitListener = uiInitListenerCaptor.getValue();
+        uiInitListener.uiInit(new UIInitEvent(ui, vaadinService));
+
+        // verifies that a scan event listener is added
+        verify(appSecServiceInstance)
+                .addScanEventListener(appSecScanEventListenerCaptor.capture());
+        var appSecScanEventListener = appSecScanEventListenerCaptor.getValue();
+        appSecScanEventListener.scanCompleted(appSecScanEvent);
+
+        // verifies that a UI is notified if new vulnerabilities are found
+        verify(ui).access(commandCaptor.capture());
+        var command = commandCaptor.getValue();
+        command.execute();
+        var constructedList = notification.constructed();
+        var constructed = constructedList.get(0);
+        verify(appSecScanEvent, times(2)).getNewVulnerabilities();
+        assertEquals(1, constructedList.size());
+        verify(constructed, times(1)).open();
+
+        // verifies that a session destroy listener is added and
+        // the scan event registration is removed when the session is destroyed
+        verify(vaadinService).addSessionDestroyListener(
+                sessionDestroyListenerCaptor.capture());
+        var sessionDestroyListener = sessionDestroyListenerCaptor.getValue();
+        sessionDestroyListener.sessionDestroy(
+                new SessionDestroyEvent(vaadinService, vaadinSession));
+        verify(scanEventRegistration, times(1)).remove();
+
+        // verifies that vulnerability and automatic scans are triggered
+        verify(appSecServiceInstance, times(1)).scanForVulnerabilities();
+        verify(appSecServiceInstance, times(1)).scheduleAutomaticScan();
     }
 
     @Test
@@ -82,5 +172,9 @@ public class AppSecServiceInitListenerTest {
         listener.serviceInit(new ServiceInitEvent(vaadinService));
 
         verify(appSecServiceInstance, never()).init();
+        verify(vaadinService, never()).addUIInitListener(any());
+        verify(vaadinService, never()).addSessionDestroyListener(any());
+        verify(appSecServiceInstance, never()).scanForVulnerabilities();
+        verify(appSecServiceInstance, never()).scheduleAutomaticScan();
     }
 }
