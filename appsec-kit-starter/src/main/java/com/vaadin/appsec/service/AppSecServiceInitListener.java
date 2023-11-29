d694d3a1e104a30fbd3bc66ba7428fc5905370ac
Tamas Mak <tamas@vaadin.com>
2023-11-29 14:56:02 +0100

    fix: Automatically enable push when AppSec is initialized (#136)

--- appsec-kit-starter/src/main/java/com/vaadin/appsec/service/AppSecServiceInitListener.java
+++ appsec-kit-starter/src/main/java/com/vaadin/appsec/service/AppSecServiceInitListener.java
@@ -8,15 +8,21 @@
  */
 package com.vaadin.appsec.service;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.appsec.backend.AppSecService;
 import com.vaadin.appsec.views.AppSecView;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.RouteConfiguration;
 import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.UIInitEvent;
 import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.shared.communication.PushMode;
 
 /**
  * A Vaadin service listener for registering the AppSec Kit route and
@@ -28,11 +34,15 @@ public class AppSecServiceInitListener implements VaadinServiceInitListener {
             .getLogger(AppSecServiceInitListener.class);
 
     private static final String APPSEC_KIT_ROUTE = "vaadin-appsec-kit";
+    private static final AtomicBoolean pushWarningShown = new AtomicBoolean(
+            false);
 
     @Override
     public void serviceInit(ServiceInitEvent event) {
-        if (isDebugMode(event.getSource())) {
+        VaadinService vaadinService = event.getSource();
+        if (isDebugMode(vaadinService)) {
             registerRoute();
+            vaadinService.addUIInitListener(this::addAfterNavigationListener);
             AppSecService appSecService = AppSecService.getInstance();
             appSecService.init();
             LOGGER.info("AppSec Kit initialized");
@@ -52,5 +62,38 @@ public class AppSecServiceInitListener implements VaadinServiceInitListener {
 
     private boolean isDebugMode(VaadinService vaadinService) {
         return !vaadinService.getDeploymentConfiguration().isProductionMode();
+    }
+
+    private void addAfterNavigationListener(UIInitEvent event) {
+        event.getUI().addAfterNavigationListener(this::checkForPush);
+    }
+
+    private void checkForPush(AfterNavigationEvent event) {
+        UI ui = event.getLocationChangeEvent().getUI();
+        if (isAppSecView(ui) && !canPushChanges(ui) && isActivationEnabled()) {
+            ui.getPushConfiguration().setPushMode(PushMode.AUTOMATIC);
+
+            boolean warningAlreadyShown = pushWarningShown.getAndSet(true);
+            if (!warningAlreadyShown) {
+                LOGGER.warn(
+                        "Server push has been automatically enabled so updates can be shown immediately. "
+                                + "Add @Push annotation on your AppShellConfigurator class to suppress this warning. "
+                                + "Set automaticallyActivatePush to false in AppSecConfiguration if you want to ensure push is not automatically enabled.");
+            }
+        }
+    }
+
+    private boolean isAppSecView(UI ui) {
+        return ui.getCurrentView() instanceof AppSecView;
+    }
+
+    private boolean canPushChanges(UI ui) {
+        return ui.getPushConfiguration().getPushMode().isEnabled()
+                || ui.getPollInterval() > 0;
+    }
+
+    private boolean isActivationEnabled() {
+        return AppSecService.getInstance().getConfiguration()
+                .isAutomaticallyActivatePush();
     }
 }
